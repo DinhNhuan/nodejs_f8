a0e59ba98ae58af2b962121a5758626dcecec33b
DinhNhuan <47028260+DinhNhuan@users.noreply.github.com>
2021-04-15 10:34:55 +0700

    add header footer

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const port = 3000;
+
+console.log(path.join(__dirname, 'public'))
+
+// static 
+app.use(express.static(path.join(__dirname, 'public')))
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// template engine
+app.engine('hbs', exphbs({
+    extname: ".hbs"
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
